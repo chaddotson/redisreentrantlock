66af8ce25f636c58c3dee24b20ecaea1acdbc900
Chad Dotson <1754094+chaddotson@users.noreply.github.com>
2024-09-23 17:50:33 -0500

    Move scratch code to main

--- main.py
+++ main.py
@@ -1,16 +1,53 @@
-# This is a sample Python script.
+import os
+import socket
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+import redis
+from redis.lock import Lock
+from redis import StrictRedis
+
+import threading
+import time as mod_time
+import uuid
+from types import SimpleNamespace, TracebackType
+from typing import Optional, Type
+
+from redis.exceptions import LockError, LockNotOwnedError
+from redis.typing import Number
+
+from redisreentrantlock import ReentrantLock
+from logging import DEBUG, INFO, basicConfig, getLogger
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+logging_config = dict(
+    level=INFO,
+    # format='%(asctime)s %(message)s'
+    format='[%(asctime)s - %(filename)s:%(lineno)d - %(funcName)s - %(levelname)s] %(message)s'
+)
+
+basicConfig(**logging_config)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+r = redis.Redis(host='localhost', port=6379, db=0)
+
+# r.lock('test', lock_class=ReentrantLock)
+# r.hset('test', mapping={'a': 1})
+
+# print(r.hget('test', 'tocken'))
+
+# r.expire('test', 5)
+
+from time import sleep
+
+with r.lock('test', lock_class=ReentrantLock):
+    print('level 1')
+    sleep(10)
+    with r.lock('test', lock_class=ReentrantLock):
+        print('level 2')
+        sleep(20)
+    print('level 1')
+    sleep(10)
+
+# r.delete('test')
